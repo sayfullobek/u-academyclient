3cd22a3bce1240bee404d476dca389b723097fad
sayfullobek__ <107360172+sayfullobek@users.noreply.github.com>
2023-09-05 23:56:34 -0700

    Update BaseUrl.ts

--- src/service/BaseUrl.ts
+++ src/service/BaseUrl.ts
@@ -1,1 +1,2 @@
-export const BASE_URL = "http://localhost:9000/api/v1"
+// export const BASE_URL = "http://localhost:9000/api/v1"
+export const BASE_URL = "https://u-academy-9b73712fd6f4.herokuapp.com/api/v1"
